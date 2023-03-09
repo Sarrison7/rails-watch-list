1b7d682bddd025cacc61d01fd8e4bca359cfd5f8
Simon Harrison <simonsy7@aol.com>
2023-03-09 14:37:14 +0000

    watchlist

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,4 @@
+class PagesController < ApplicationController
+  def watch_list
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  get '/movies", to: "pages#movies'
   # Defines the root path route ("/")
   # root "articles#index"
 end
